8361feaed392a791a04ae825a85b0fcbdc590395
api2024_2 <carlosryr24@gmail.com>
2024-12-13 15:39:51 -0500

    se actualizo todo

--- src/routes/perfil.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { obtenerPerfiles } from '../controladores/perfil.Ctrl.js';
-
-const router = Router();
-
-router.get('/', obtenerPerfiles); // Obtener todos los perfiles
-
-export default router;
-
